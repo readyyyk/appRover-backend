bceffe8b2413e4ffa618dba7dcc79c2030f03240
halone <haloneprog@gmail.com>
2024-01-26 22:01:47 +0300

    [-] Run scripts

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from dotenv import load_dotenv
-load_dotenv('.env.local')
+from uvicorn import run
+from pytest import main
+from os import getcwd
+from pathlib import Path
+env_local_path = Path(getcwd()) / '.env.local'
 
-from approver_backend.api import app
 
+def run_dev():
+    load_dotenv(env_local_path)
+    from approver_backend.api import app
+    run(app)
+
+
+def run_tests():
+    load_dotenv(env_local_path)
+    main()
