86466a6aec38ec0383d97e8ae1b7ce996605571f
halone <haloneprog@gmail.com>
2024-01-30 15:59:53 +0300

    [F] Cors added

--- approver_backend/api/__init__.py
+++ approver_backend/api/__init__.py
@@ -7,6 +7,7 @@ from .core import app as main_router
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from approver_backend.database.core import init_database
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -17,6 +18,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     host='0.0.0.0',
     lifespan=lifespan
+)
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(main_router)
 
